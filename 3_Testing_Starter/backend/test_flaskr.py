220ca1537a34b35bd8f9343eb132cb92a4d7a33b
antoniofurtado <antoniordf@hotmail.com>
2022-04-04 20:05:29 +0100

    Implemented tests.

--- 3_Testing_Starter/backend/test_flaskr.py
+++ 3_Testing_Starter/backend/test_flaskr.py
@@ -40,7 +40,35 @@ class BookTestCase(unittest.TestCase):
 #        Since there are four routes currently, you should have at least eight tests.
 # Optional: Update the book information in setUp to make the test database your own!
 
+    def test_retrieve_books_success(self):
+        '''This function tests if @app.route('/books') / def retrieve_books() endpoint returns successfully'''
 
+        res = self.client().get('/books')
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_retrieve_books_error(self):
+        '''This func tests if @app.route('/books') / def retrieve_books() endpoint returns an error'''
+
+        res = self.client().get('/books')
+
+        self.assertEqual(res.status_code, 404)
+
+    def test_update_book_success(self):
+        '''This function tests if @app.route('/books/<int:book_id>') / def update_book() endpoint returns successfully'''
+
+        res = self.client().get('/books/<int:book_id>')
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_update_book_error(self):
+        '''This function tests if @app.route('/books/<int:book_id>') / def update_book() endpoint returns an error'''
+
+        res = self.client().get('/books/<int:book_id>')
+
+        self.assertEqual(res.status_code, 404)
+
+    
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
